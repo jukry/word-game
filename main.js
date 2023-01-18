5a88a1b1080de4b762cc1346bacf7924dd3fe55f
jukry <94563540+jukry@users.noreply.github.com>
2023-01-18 09:59:48 +0200

    word checking works, added winning statement

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const page = document
 const API_ENDPOINT = "https://words.dev-apis.com/word-of-the-day/"
-const grid = document.getElementsByClassName("grid1")
+const grid = document.getElementsByClassName("grid-item")
 const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+const infoEl = document.getElementById("info")
+let tryNum = 0
 
 async function fetchWord(url) {
     const response = await fetch(url)
@@ -10,23 +12,30 @@ async function fetchWord(url) {
     return word
 }
 
-let wordToGuess = await fetchWord(API_ENDPOINT)
+// let wordToGuess = await fetchWord(API_ENDPOINT)
+let wordToGuess = "await"
 console.log(wordToGuess)
+
 page.addEventListener("keydown",(e) => {
-    if (alphabet.includes(e.key.toUpperCase()) || e.key == "Enter") {
+    if (alphabet.includes(e.key.toUpperCase()) || e.key == "Enter" || e.key == "Backspace") {
         doStuff(e.key)
     }
 })
 
-const wordArr = []
+let wordArr = []
 function doStuff(keyPressed) {
-    if (keyPressed == "Enter") {
+    const gridElement = grid[tryNum].children
+    if (keyPressed == "Backspace") {
+        // remove last letter
+        wordArr.pop()
+        gridElement[wordArr.length].innerHTML = ""
+    } else if (keyPressed == "Enter") {
+        // check if word is correct or has matching letters
         checkWord()
-        return
-    } 
-    
-    if (wordArr.length < 5) {
-        grid[0].children[wordArr.length].innerHTML = keyPressed
+        tryNum++
+        wordArr = []
+    } else if (wordArr.length < 5) {
+        gridElement[wordArr.length].innerHTML = keyPressed.toUpperCase()
         wordArr.push(keyPressed)
     }
 }
@@ -34,9 +43,31 @@ function doStuff(keyPressed) {
 function checkWord() {
     let word = wordArr.toString().replaceAll(",","")
     if (word == wordToGuess) {
-        // winner, do something
+        checkLetters(word)
+        infoEl.innerHTML = `Correct, the word is ${wordToGuess}`
+        page.removeEventListener("keydown")
+        return
     } else {
-        // not correct, do something
+        checkLetters(word)
     }
-    
+}
+
+function checkLetters(wordFromCheckWord) {
+    for (let i = 0;i < wordFromCheckWord.length;i++) {
+        const gridElement = grid[tryNum].children[i]
+
+        if (wordFromCheckWord[i] == wordToGuess[i]) {
+            gridElement.classList.toggle("correct")
+            
+        } else if (wordToGuess.includes(wordFromCheckWord[i])) {
+            console.log(wordToGuess.includes(wordFromCheckWord[i]),wordFromCheckWord[i])
+            for (let j = 0; j < wordToGuess.length; j++) {
+                gridElement.classList.toggle("includes")
+                
+            }
+            // pitää tarkistaa löytyykö sanasta jostain x kirjain jostain muusta kohtaa
+            console.log(`${wordFromCheckWord[i]} ${wordToGuess.split(wordFromCheckWord[i]).length-1}`) // etsi indeksi kirjaimelle
+            
+        }
+    }
 }
